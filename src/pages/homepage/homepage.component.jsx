776d940095560acf2b0966dba14ec6750a91cf05
Rinnohara9803 <sagarprajapati9803@gmail.com>
2022-11-07 15:02:54 +0545

    menu items created

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DirectoryMenu from "../../components/directory-menu/directory-menu.component.jsx";
+import "./homepage.styles.scss";
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+};
